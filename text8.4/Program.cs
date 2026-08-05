5de6cf169e441628b2c44a3e788cf4523cdf6d7a
MYN <3456482186@qq.com>
2026-08-05 19:50:36 +0800

    111

---
+++ text8.4/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace text8._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          /*  // 定义：指定键值对的类型
+            Dictionary<string, dynamic> person = new Dictionary<string, dynamic>()
+            {
+                ["name"] = "张三",
+                ["age"] = 20,
+                ["isMan"] = true,   
+                ["wife"] = new Dictionary<string, dynamic>()
+                {
+                    ["name"] = "翠花",
+                    ["age"] = 12
+                }
+            };
+            person["wife"]["name"]= "小翠";
+            Console.WriteLine(person["name"]);
+            Console.WriteLine(person["wife"]["name"]);*/
+
+            /*List<string> lenth = new List<string>()
+            {
+                "A",
+                "B",
+                "C",
+                "D"
+            };
+            lenth.Add("Z");
+            lenth.Insert(2, "X");
+            lenth.AddRange([ "Q", "W", "E" ]);
+            lenth.RemoveRange(5, 4);
+            Console.WriteLine(lenth.Count);*/
+
+            /* List<string> lenth = new List<string>()
+             {
+                 "张三",
+                 "李四",
+                 "王五",
+                 "赵六",
+                 "甲",
+                 "乙",
+                 "丁"
+             };
+             Random r = new Random();
+             int i = r.Next(lenth.Count);
+             Console.WriteLine(lenth[i]);
+             //Console.WriteLine(lenth[r.Next(lenth.Count)]);*/
+        }
+    }
+}
