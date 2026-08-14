6f83e49ccab19b6aa0ba4604faa4cdba6b730dc1
MYN <3456482186@qq.com>
2026-08-14 17:53:21 +0800

    文件I/O

--- 8.14test/Program.cs
+++ 8.14test/Program.cs
@@ -4,7 +4,62 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            /*Action writeLog = () =>
+            {
+                Console.WriteLine("输入：");
+                string opt = Console.ReadLine();
+                var aaa = @"./content.log";
+                File.WriteAllText(aaa,$"{opt}\t"+DateTime.Now);
+            };
+            writeLog();*/
+
+           /* Func<string, int> isFileorDir = path =>
+            {
+                // 说明path是文件
+                if (File.Exists(path)) return 1;
+                // 说明path是目录
+                if (Directory.Exists(path)) return 2;
+                return 0;
+            };
+            string[] resArr = ["NO", "isfile", "isDir"];
+            //int res = isFileorDir("./");
+            //int res = isFileorDir("./content.log");
+            //Console.WriteLine(resArr[res]);
+
+            Func<string, List<string>> getFileAndDir = path =>
+            {
+                List<string> resList = [];
+                // 判断路径是否是目录,如果不是目录则 手动抛出一个异常
+                if (isFileorDir(path) != 2) throw new Exception("传递的参数有误,必须要是目录路径");
+                // 获取目录下的所有文件
+                string[] files = Directory.GetFiles(path);
+                resList.AddRange(files);
+                // 获取所有的目录
+                string[] dirs = Directory.GetDirectories(path);
+                resList.AddRange(dirs);
+                return resList;
+            };
+            //var res = getFileAndDir("./");
+            var res = getFileAndDir(@"D:\视觉02");
+            foreach (var item in res) Console.WriteLine(item);*/
+
+
+            //使用读写文件配合命令行窗口  模拟实现注册功能
+            //要求输入用户名和密码,完成注册; (注册的用户信息记录在user.txt文件中, 一行一个用户信息 数据之间通过 === 分隔)
+            Action writeLog = () =>
+            {
+                Console.WriteLine("请输入用户名：");
+                string user = Console.ReadLine();
+                Console.WriteLine("请输入密码：");
+                string pwd = Console.ReadLine();
+                var aaa = @"./user.txt";
+                File.AppendAllText(aaa, $"用户名：{user} -- 密码：{pwd}\n" );
+                File.AppendAllText(aaa, $"=======================\n" );
+
+            };
+            writeLog();
+
         }
     }
 }
--- 8.14test/obj/Debug/net8.0/8.14test.AssemblyInfo.cs
+++ 8.14test/obj/Debug/net8.0/8.14test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("8.14test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f05672e6b2fe99c87188e94283f9d1c8c8348f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+842ed1a6c48883b10c589d084292680f31e0987d")]
 [assembly: System.Reflection.AssemblyProductAttribute("8.14test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("8.14test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
